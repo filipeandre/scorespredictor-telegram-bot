bb7f9f10f06da8a9c2e807ecbca075685026129e
Filipe <www@filipeandre.com>
2019-03-08 12:05:14 +0000

    Allow to have multiple configs

--- scorespredictor.go
+++ scorespredictor.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gocolly/colly"
 	"github.com/tealeg/xlsx"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -13,38 +14,50 @@ func main() {
 	conf := loadConfiguration()
 	location,_ := time.LoadLocation("Europe/Rome")
 	date := time.Now().In(location).Format("2006-01-02")
-	collectors := map[string]*colly.Collector{}
-	responses := map[string]*htmlTable{}
+	respC := map[int]chan *htmlTable{}
 
 	//Start fetching data
 	for i := 0; i < len(conf.Sports); i++ {
 		name:=conf.Sports[i]
 		c := collect(conf.Url, name, date)
+		respC[i] = make(chan *htmlTable)
+		respI:=i
 
-		//Populate responses map on response
+		//Populate respC map on response
 		c.OnResponse(func(r *colly.Response) {
 			print(fmt.Sprintf("Response received, status: %d", r.StatusCode))
 			data := &htmlTable{}
 			err := json.Unmarshal(r.Body, data)
-			responses[name]  = data
+			respC[respI] <- data
 
 			if err != nil {
 				onError(err)
 			}
 		})
-		collectors[name]  = c
 	}
 
 	//Wait each response and generate the xlsx object
 	file := xlsx.NewFile()
 	db:= createDb(conf.Storage + "/tmp.db", conf.Sports)
-	filter:= conf.Filter
-	for name, c := range collectors {
-		c.Wait()
-		seedTable(name, responses[name].Html, db)
-		saveSheet(name, name, responses[name].Html, file, db, "")
-		saveSheet(name + " - filtred",name, responses[name].Html, file, db, filter)
-		sendTelegramMessage(conf.Telegram.Token, conf.Telegram.Channel, generateMarkdown(responses[name].Html, name, filter, db))
+	parts:= strings.Split(conf.Filter, ";")
+
+
+
+	for i:= 0; i< len(conf.Sports); i++{
+		name:= conf.Sports[i]
+
+		var filter string
+		if len(parts) >= i{
+			filter=parts[i]
+		}else{
+			filter=parts[0]
+		}
+
+		response := <- respC[i]
+		seedTable(name, response.Html, db)
+		saveSheet(name, name, response.Html, file, db, "")
+		saveSheet(name + " - filtred",name, response.Html, file, db, filter)
+		sendTelegramMessage(conf.Telegram.Token, conf.Telegram.Channel, generateMarkdown(response.Html, name, filter, db))
 	}
 
 	//Write file to disk
